c7d9e3c53836a8c988fd288ae74bb8bb2337626e
Gaston <corbalan.gaston@outlook.com>
2022-07-10 10:07:56 -0300

    Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sum, setSum] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Anotador de truco</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
+          <Button colorScheme='blue' type="button" onClick={() => setSum((sum) => sum + 1)}>
+            count is: {sum}
+          </Button>
         </p>
       </header>
     </div>
